661f7dc532d24c6173e7402cc15764f6131497f0
anthonycarl <smeat32@yahoo.com>
2014-06-10 00:23:11 -0400

    Add missing tests file

---
+++ src/NodaTime.Serialization.ServiceStackText.UnitTests/NodaSerializerDefinitionsTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using NodaTime.TimeZones;
+using Xunit;
+
+namespace NodaTime.Serialization.ServiceStackText.UnitTests
+{
+    [ExcludeFromCodeCoverage]
+    public class NodaSerializerDefinitionsTests
+    {
+        [Fact]
+        public void CreateZonedDateTimeSerializer_NullPattern_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () =>
+                NodaSerializerDefinitions.CreateZonedDateTimeSerializer(
+                    new DateTimeZoneCache(TzdbDateTimeZoneSource.Default),
+                    null));
+        }
+    }
+}
